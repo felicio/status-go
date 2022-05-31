9af1edc058a9306f64fc2af96ead3e71619809f3
Felicio Mununga <felicio@users.noreply.github.com>
2022-05-31 13:27:30 +0200

    change install_deps.sh

--- _assets/scripts/install_deps.sh
+++ _assets/scripts/install_deps.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-if [[ -x $(command -v apt) ]]; then
-  apt install -y protobuf-compiler jq
-elif [[ -x $(command -v pacman) ]]; then
-  pacman -Sy protobuf jq --noconfirm
-elif [[ -x $(command -v brew) ]]; then
+if [[ -x $(command -v brew) ]]; then
   brew install protobuf jq
-elif [[ -x $(command -v nix-env) ]]; then
-  nix-env -iA nixos.protobuf3_13
+# if [[ -x $(command -v apt) ]]; then
+#   apt install -y protobuf-compiler jq
+# elif [[ -x $(command -v pacman) ]]; then
+#   pacman -Sy protobuf jq --noconfirm
+# elif [[ -x $(command -v brew) ]]; then
+#   brew install protobuf jq
+# elif [[ -x $(command -v nix-env) ]]; then
+#   nix-env -iA nixos.protobuf3_13
 else
   echo "ERROR: No known package manager found!" >&2
   exit 1
